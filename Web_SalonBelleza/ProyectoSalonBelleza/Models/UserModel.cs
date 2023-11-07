06867868f7c6cf87604f108c404c5cf7b2c16c5a
Xpronoob <kevin_03_puris@hotmail.com>
2023-11-07 03:38:55 -0600

    Authentication + SMTP

---
+++ Web_SalonBelleza/ProyectoSalonBelleza/Models/UserModel.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using ProyectoSalonBelleza.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Web;
+
+namespace ProyectoSalonBelleza.Models
+{
+    public class UserModel
+    {
+
+        public string rutaServidor = ConfigurationManager.AppSettings["RutaApi"];
+
+        public UserEnt IniciarSesion(UserEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "IniciarSesion";
+                var jsonData = JsonContent.Create(entidad);
+                var res = client.PostAsync(urlApi, jsonData).Result;
+                return res.Content.ReadFromJsonAsync<UserEnt>().Result;
+            }
+        }
+
+        public string RegistrarCuenta(UserEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "RegistrarCuenta";
+                var jsonData = JsonContent.Create(entidad);
+                var res = client.PostAsync(urlApi, jsonData).Result;
+                return res.Content.ReadFromJsonAsync<string>().Result;
+            }
+        }
+
+        public string RecuperarCuenta(UserEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "RecuperarCuenta?email=" + entidad.email;
+                var res = client.GetAsync(urlApi).Result;
+                return res.Content.ReadFromJsonAsync<string>().Result;
+            }
+        }
+    }
+}
